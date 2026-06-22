72fd9709d5606d0031dcfb14c6c0f5f403ad6bce
Nathan Moody <146941482+frumpyyy@users.noreply.github.com>
2026-06-22 11:33:51 +0100

    getting core game loop up and running
    currently game idea is that a catalyst as in used within physics to speed up a reaction, is fired by the user hitting atoms into each other to make them fuse together, once fused they combine and if hit into again by another atom that one will explode pushing other atoms into each other causing a chain like reaction

--- Assets/Scripts/LevelScripts/LevelManager.cs
+++ Assets/Scripts/LevelScripts/LevelManager.cs
@@ -1,16 +1,58 @@
+using System.Collections;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static LevelManager m_instance { get; private set; }
+
+    private int _atomCount;
+
+    private int _catalystCount;
+
+    private void Awake()
     {
-        
+        if (m_instance != null && m_instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        m_instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        Atom.atomFused += reactionCompleted;
+    }
+
+    private void OnDisable()
+    {
+        Atom.atomFused -= reactionCompleted;
+    }
+
+    public void Start()
+    {
+        GameManager.m_instance.UpdateGameState(GameManager.LevelState.InPlay);
+    }
+
+    public void atomRegister() => _atomCount++;
+
+    public void reactionCompleted()
+    {
+        _atomCount--;
+
+        if (_atomCount == 0)
+            StartCoroutine(WinScreen());
+    }
+
+    private IEnumerator WinScreen()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GameManager.m_instance.UpdateGameState(GameManager.LevelState.Won);
+    }
+
+    private IEnumerator LossScreen()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GameManager.m_instance.UpdateGameState(GameManager.LevelState.Lost);
     }
 }
--- Assets/Scripts/LevelScripts/UIManager.cs
+++ Assets/Scripts/LevelScripts/UIManager.cs
@@ -23,7 +23,13 @@ public class UIManager : MonoBehaviour
 
     public void StateChanged(GameManager.LevelState newState)
     {
+        //i was going to do a switch statement here however in the off chance that UI is incorrectly triggered the switch statement would only turn on
+        //the win or lose ui not turning the opposite off, meaning code would have to be duplicated within the switch to perform this funcitonality.
+        bool gameWon = newState == GameManager.LevelState.Won;
+        bool gameLost = newState == GameManager.LevelState.Lost;
 
+        winUI.SetActive(gameWon);
+        loseUI.SetActive(gameLost);
     }
 
     //functions for UI buttons to restart lvl and skip to next
